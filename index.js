0f45b323c2328db5e6326bc89d632bbe83f52d52
Alabê Duarte <alabeduarte@gmail.com>
2015-07-25 09:40:08 -0300

    Setup for mypodcasts stub api

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+var fs = require("fs");
+
+var fromFile = function(fileName) {
+  return fs.readFileSync(__dirname + fileName, 'utf-8');
+};
+
+var jsonParse = function(file) {
+  return JSON.parse(file);
+};
+
+app.get('/api/user/johndoe/latest_episodes', function(req, res) {
+  var json = jsonParse(fromFile('/latest_episodes.json'));
+
+  res.json(json)
+});
+
+app.get('/api/user/johndoe/feeds', function(req, res) {
+  var json = jsonParse(fromFile('/user_feeds.json'));
+
+  res.json(json)
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
